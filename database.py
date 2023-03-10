8cb76aaa9d240a2b0ee863ffc2053657adc8c49e
zachibs <zachibsn@gmail.com>
2023-03-10 13:39:33 +0200

    created the database file and functions

---
+++ database.py
@@ -0,0 +1,109 @@
+from sqlalchemy import create_engine, ForeignKey, Column, String, Integer, CHAR, Boolean
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+
+
+Base = declarative_base()
+class Country(Base):
+    __tablename__ = "Countries"
+
+    name = Column("name", String, primary_key=True)
+    population = Column("population", Integer)
+    ethnicity = Column("ethnicity", String)
+    founding_year = Column("founding_year", Integer)
+    in_war = Column("in_war", Boolean)
+
+
+    def __init__(self, name: str, population: int, ethnicity: str, founding_year: int , in_war: bool) -> None:
+        self.name = name
+        self.population = population
+        self.ethnicity = ethnicity
+        self.founding_year = founding_year
+        self.in_war = in_war
+    
+    def __repr__(self) -> str:
+        return f"(Country: {self.name}, current population: {self.population}, main ethnicity group: {self.ethnicity}, founded in {self.founding_year}, in war={self.in_war})"
+
+    def to_dict(self) -> dict:
+        return {"name": self.name, "population": self.population,
+                "ethnicity":self.ethnicity, "founding_year": self.founding_year,
+                "in_war": self.in_war}
+
+def get_session():
+    try:
+        engine = create_engine("sqlite:///mydb.db", echo=False)
+    except Exception as e:
+        print("\n\Exception in creating database engine\n\n")
+        return
+    
+    Base.metadata.create_all(bind=engine)
+    Session = sessionmaker(bind=engine)
+    session_instance = Session()
+    return session_instance
+    
+
+
+def add_value_to_db(session_instance, name: str, population: int, ethnicity: str, founding_year: int , in_war: bool) -> None:
+    country_instance = Country(name, population, ethnicity, founding_year, in_war)
+    try:
+        if (get_country_by_name(session_instance, name)):
+            print("Country already exist")
+            return
+        session_instance.add(country_instance)
+        session_instance.commit()
+    except Exception as e:
+        session_instance.rollback()
+
+def get_all_countries(session_instance):
+    result = session_instance.query(Country).all()
+    return result
+
+def get_country_by_name(session_instance, country_name: str):
+    result = session_instance.query(Country).filter(Country.name == country_name).first()
+    return result
+
+def get_countries_by_ethnicity(session_instance, ethnicity: str):
+    result = session_instance.query(Country).filter(Country.ethnicity == ethnicity).all()
+    return result
+
+def get_counties_by_war_status(session_instance, in_war: bool):
+    result = session_instance.query(Country).filter(Country.in_war == in_war).all()
+    return result
+
+def get_counties_with_population_over_number(session_instance, min_number_of_people: int):
+    result = session_instance.query(Country).filter(Country.population >= min_number_of_people).all()
+    return result
+
+
+def get_countries_founded_after_year(session_instance, year):
+    result = session_instance.query(Country).filter(Country.founding_year > year).all()
+    return result
+
+def change_country_name(session_instance, old_name: str, new_name: str):
+    try:
+        session_instance.query(Country).filter(Country.name == old_name).update({"name": new_name})
+        session_instance.commit()
+    except Exception as e:
+        print("Failed to update country's name")
+
+def change_country_war_status(session_instance, name: str, new_in_war: str):
+    try:
+        session_instance.query(Country).filter(Country.name == name).update({"in_war": new_in_war})
+        session_instance.commit()
+    except Exception as e:
+        print("Failed to update country's war status")
+
+def change_country_population(session_instance, name: str, new_population: int):
+    try:
+        session_instance.query(Country).filter(Country.name == name).update({"population": new_population})
+        session_instance.commit()
+    except Exception as e:
+        print("Failed to update country's population")
+
+if __name__ == "__main__":
+    session = get_session()
+    add_value_to_db(session, "Israel", 8000000, "Mixed", 1948, False)
+    add_value_to_db(session, "Bolivia", 4000000, "Mixed", 1948, False)
+    list_of_countries = [x.to_dict() for x in list(get_all_countries(session))]
+    print(list_of_countries)
+
