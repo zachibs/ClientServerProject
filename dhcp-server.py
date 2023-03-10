dd66cdcd6b2f91a21d0bd62759ddffe73bc22c24
zachibs <zachibsn@gmail.com>
2023-03-10 21:41:39 +0200

    changed the server file name, added comments to dhcp

--- dhcp-server.py
+++ dhcp-server.py
@@ -1,3 +1,4 @@
+# Import necessary modules from Scapy
 from scapy.all import *
 from scapy.layers.dhcp import DHCP, BOOTP
 from scapy.layers.inet import IP, UDP
@@ -13,16 +14,19 @@ DPORT = 68
 SPORT = 67
 ONE_HOUR_IN_SECONDS = 600
 
+# Function to handle incoming DHCP packets
 def dhcp_server(pkt):
+    # Get MAC address of the network interface
     server_mac_address = get_if_hwaddr(NETWORK_INTERFACE)
+    # Get MAC address of the client requesting DHCP
     client_mac_address = pkt[Ether].src
 
     if pkt[DHCP]:
-
-        # check if it's a DHCP Discover message
+        # Check if it's a DHCP Discover message
         if pkt[DHCP].options[0][1] == 1:
+            # Print message indicating that DHCP Discover message has been received
             print("DHCP Discover received from client: {}".format(client_mac_address))
-            # create DHCP offer packet
+            # Create DHCP offer packet
             dhcp_offer = Ether(src=server_mac_address, dst=client_mac_address)/ \
                          IP(src=SERVER_IP, dst=CLIENT_IP)/ \
                          UDP(sport=SPORT, dport=DPORT)/ \
@@ -30,14 +34,16 @@ def dhcp_server(pkt):
                          DHCP(options=[('message-type', 'offer'), ('subnet_mask', MASK_SUBNET),
                                        ('router', SERVER_IP), ('name_server', SERVER_IP),
                                        ('lease_time', ONE_HOUR_IN_SECONDS)])
-
+            # Print message indicating that DHCP Offer message is being sent
             print("Sending DHCP Offer to client: {}".format(client_mac_address))
+            # Send DHCP Offer packet to the client
             sendp(dhcp_offer, iface=NETWORK_INTERFACE)
             
-        # check if it's a DHCP Request message
+        # Check if it's a DHCP Request message
         elif pkt[DHCP].options[0][1] == 3:
+            # Print message indicating that DHCP Request message has been received
             print("DHCP Request received from client: {}".format(client_mac_address)) 
-            # create DHCP ACK packet
+            # Create DHCP ACK packet
             dhcp_ack = Ether(src=server_mac_address, dst=client_mac_address)/ \
                        IP(src=SERVER_IP, dst=BROADCAST)/ \
                        UDP(sport=SPORT, dport=DPORT)/ \
@@ -45,11 +51,14 @@ def dhcp_server(pkt):
                        DHCP(options=[('message-type', 'ack'), ('subnet_mask', MASK_SUBNET),
                                      ('router', SERVER_IP), ('name_server', SERVER_IP),
                                      ('lease_time', ONE_HOUR_IN_SECONDS)])
-
+            # Print message indicating that DHCP ACK message is being sent
             print("Sending DHCP ACK to client: {}".format(client_mac_address))
+            # Send DHCP ACK packet to the client
             sendp(dhcp_ack, iface=NETWORK_INTERFACE)
-        
 
+# Main function
 if __name__ == '__main__':
+    # Print message indicating that the DHCP server is starting
     print("Starting DHCP server...")
+    # Sniff for DHCP packets on the specified network interface
     sniff(filter='udp and (port 67 or 68)', prn=dhcp_server ,iface=NETWORK_INTERFACE)
