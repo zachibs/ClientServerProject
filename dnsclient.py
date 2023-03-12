a76da1e1215fe48c7ad312b6816c3da7d025461a
zachibs <zachibsn@gmail.com>
2023-03-12 02:39:22 +0200

    rollback dns to sniffer

--- dnsclient.py
+++ dnsclient.py
@@ -1,33 +1,43 @@
-import socket
-import json
-from config import *
+from scapy.all import *
+from scapy.sendrecv import sniff, send
 from scapy.layers.inet import UDP, IP
 from scapy.layers.dns import DNS, DNSQR, DNSRR
-from scapy.all import RandShort
+from time import sleep
+
+from config import *
+
+app_server_ip = 0
+
+def send_dns_request(app_domain_to_find, new_client_ip_address, dns_server_ip_address):
+    packet = IP(src=new_client_ip_address, dst=dns_server_ip_address) / \
+             UDP(sport=RandShort(), dport=DNS_SPORT) / \
+             DNS(rd=1, qd=DNSQR(qname=app_domain_to_find))
+    send(packet)
+
+def handle_dns_packet(pkt):
+    sleep(1)
+    global app_server_ip
+    if not pkt[IP]:
+        return
+
+    ip_source = pkt[IP].src
+    ip_dest = pkt[IP].dst
+    
+    if not pkt.haslayer(DNS):
+        return
+    if not pkt.haslayer(DNSRR):
+        return
+
+    app_server_URL = pkt[DNSRR].rrname.decode().rstrip(".")
+
+    if app_server_URL != APP_DOMAIN:
+        return
+
+    app_server_ip = pkt[DNSRR].rdata
 
 
-def query_dns_server_for_ip(domain_name, new_client_ip_address, dns_server_ip_address):
-    print("Started DNS client")
-    # Create a UDP socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-    # Encode the DNS query
-    # request_json = json.dumps({"domain":domain_name})
-    # query = request_json.encode()
-
-    packet = DNS(rd=1, qd=DNSQR(qname=domain_name))
-
-    # query = (str(packet)).encode()
-    query = bytes(packet)
-    sock.sendto(query, (DNS_SERVER_IP, DNS_SPORT))
-
-    # Receive the DNS response
-    data, addr = sock.recvfrom(1024)
-
-    # Decode and print the DNS response
-    dns_data = (DNS(data))
-    app_ip_address = dns_data.an.rdata
-    print(f"Received DNS response from {addr}: Domain: '{domain_name}' , ip-address: {app_ip_address}")
-    if app_ip_address == "0.0.0.0":
-        return None
-    return app_ip_address
+def query_dns_server_for_ip(app_domain_to_find, new_client_ip_address, dns_server_ip_address):
+    send_dns_request(app_domain_to_find, new_client_ip_address, dns_server_ip_address) 
+    sniff(filter=f"udp port 53 and src {DNS_SERVER_IP}", count=2, timeout=10, prn=handle_dns_packet, iface=NETWORK_INTERFACE)
+    print(app_server_ip)
+    return app_server_ip
