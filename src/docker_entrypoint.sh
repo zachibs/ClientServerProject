717289f0ca19cf28dab78774b066f26579157872
zachibs <zachibsn@gmail.com>
2023-04-15 16:54:45 +0300

    update to entrypoint

--- src/docker_entrypoint.sh
+++ src/docker_entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-exec sudo python3 -u src/servers/dhcp_server.py &
-exec sudo python3 -u src/servers/dns_server.py &
-exec sudo python3 -u src/servers/app_server.py &
+exec sudo python3 -u servers/dhcp_server.py &
+exec sudo python3 -u servers/dns_server.py &
+exec sudo python3 -u servers/app_server.py &
